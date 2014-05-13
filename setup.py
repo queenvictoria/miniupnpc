3cc576873bf6126417fdecd076c1d0dc5884c511
Chen Houwu <chenhouwu@gmail.com>
2014-05-13 14:27:40 +0800

    run make if needed

--- setup.py
+++ setup.py
@@ -8,6 +8,15 @@
 # replace libminiupnpc.a by libminiupnpc.so for shared library usage
 from distutils.core import setup, Extension
 from distutils import sysconfig
+
+import os
+import subprocess
+
+# build static lib if not exists
+prj_root = os.path.dirname(__file__)
+if not os.path.exists(os.path.join(prj_root, "libminiupnpc.a")):
+    subprocess.call(['make', 'upnpc-static'])
+
 sysconfig.get_config_vars()["OPT"] = ''
 sysconfig.get_config_vars()["CFLAGS"] = ''
 setup(name="miniupnpc",
